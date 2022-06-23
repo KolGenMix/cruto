e8530822c5a9701ffcd728520c869df22dbc9756
Gena <mihakolot@mail.ru>
2022-06-23 09:08:01 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -269,4 +269,107 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     window.addEventListener('scroll', showModalByScroll);
 
+
+
+    // используем классы для карточек
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.transfer = 27;
+            this.changeToUAN();
+
+            this.parent = document.querySelector(parentSelector);
+            this.classes = classes;
+
+        }
+
+        changeToUAN() {
+            this.price = this.price * this.transfer;
+        }
+        // в методе render у нас  div class =
+        // menu__item, мы не знаем сколько у нас будет карточек.
+        // мы в аргументы MenuCard добавляем classes,как rest .
+        // убираем класс menu__item, с render перебираем classes.forEach.
+        //  и menu__item добавляем к каждой
+        //  new MenuCard в конце
+        render() {
+            const element = document.createElement('div');
+
+            this.classes.forEach(className => {
+                element.classList.add(className);
+            });
+            element.innerHTML = `
+                
+                <img src= ${this.src} alt= ${this.alt}>
+                <h3 class="menu__item-subtitle"> ${this.title}</h3>
+                <div class="menu__item-descr"> ${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                   <div class="menu__item-cost">Цена:</div>
+                   <div class="menu__item-total"><span> ${this.price}</span> грн/день</div>
+                </div>
+             `;
+
+            // теперь когда мы создали элемент,нам нам нужно найти родителя и поместить его туда.
+            // мы напишем его в конструктор и напишем как parentSelector 
+
+
+            this.parent.append(element);
+
+        }
+
+
+
+    }
+
+    // можно к render обратится
+
+    // 1 const div = new MenuCard();
+    //   div.render();
+
+    // 2 new MenuCard().render();
+
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        // и затем прописываем parentSelector.то есть родителя из html.мы находимся в классе menu и conteiner
+        '.menu .container',
+        'menu__item'
+    ).render();
+    // мы создали конструктор, метод render,и теперь создаем новые карточки new
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты,фрукты - ресторанное меню без похода в ресторан!',
+        14,
+        '.menu .container',
+        'menu__item'
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        21,
+        '.menu .container',
+        'menu__item'
+    ).render();
+
+    // создав динамически эти карточки ,убираем их с верстки в html
+
+
+
+
 });
